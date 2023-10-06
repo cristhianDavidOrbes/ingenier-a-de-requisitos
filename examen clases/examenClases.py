a15863f60b3e515ba85ee8c9d5780fcf66a98132
cristhianDavidOrbes <134344774+cristhianDavidOrbes@users.noreply.github.com>
2023-10-06 09:37:03 -0500

    Add files via upload

---
+++ examen clases/examenClases.py
@@ -0,0 +1,20 @@
+def suma(val1,val2):
+    sumar = val1 + val2
+    print(sumar)
+def resta(val1,val2):
+    restar = val1 - val2
+    print(restar)
+def multiplicacion(val1,val2):
+    multiplicar = val1 * val2
+    print(multiplicar)
+def dividicion(val1,val2):
+    dividir = val1 /val2
+    print(dividir)
+print("calculadora: ")
+valor1 = float(input("porfavor digite el primer valor: "))
+valor2 = float(input("porfavor digite el primer valor: "))
+suma(valor1,valor2)
+resta(valor1,valor2)
+multiplicacion(valor1,valor2)
+dividicion(valor1,valor2)
+input("presione cualquier tecla para terminar")
